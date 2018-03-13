f7d9a5e39bd7c4134ae28a276ba62d6680510d33
smike86 <mike.smike86@gmail.com>
2018-03-13 01:27:47 +0100

    implemeted IA

--- src/main/java/it/unipr/scarpenti/ant/ui/GamePlay.java
+++ src/main/java/it/unipr/scarpenti/ant/ui/GamePlay.java
@@ -18,6 +18,11 @@ import it.unipr.scarpenti.ant.Direction;
 import it.unipr.scarpenti.ant.Position;
 import it.unipr.scarpenti.ant.exception.AntGameException;
 import weka.classifiers.trees.J48;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
 import weka.core.SerializationHelper;
 
 public class GamePlay extends JPanel implements KeyListener {
@@ -182,10 +187,95 @@ public class GamePlay extends JPanel implements KeyListener {
 
 		boolean posChanged = false;
 		if (appData.getWhoPlay().equals(AppData.PLAYER_IA)) {
-			if (arg0.getKeyCode() == KeyEvent.VK_ENTER ||arg0.getKeyCode() == KeyEvent.VK_SPACE) {
+			if (arg0.getKeyCode() == KeyEvent.VK_ENTER || arg0.getKeyCode() == KeyEvent.VK_SPACE) {
 				posChanged = true;
-			}
-		} else {
+
+				// Declare two numeric attributes
+				Attribute Attribute1 = new Attribute("index-0-0");
+				Attribute Attribute2 = new Attribute("index-0-1");
+				Attribute Attribute3 = new Attribute("index-0-2");
+
+				Attribute Attribute4 = new Attribute("index-1-0");
+				Attribute Attribute5 = new Attribute("index-1-1");
+				Attribute Attribute6 = new Attribute("index-1-2");
+
+				Attribute Attribute7 = new Attribute("index-2-0");
+				Attribute Attribute8 = new Attribute("index-2-1");
+				Attribute Attribute9 = new Attribute("index-2-2");
+
+				// Declare the class attribute along with its values
+				FastVector fvClassVal = new FastVector(4);
+				fvClassVal.addElement("UP");
+				fvClassVal.addElement("DOWN");
+				fvClassVal.addElement("RIGTH");
+				fvClassVal.addElement("LEFT");
+				Attribute ClassAttribute = new Attribute("direction", fvClassVal);
+
+				// Declare the feature vector
+				FastVector fvWekaAttributes = new FastVector(10);
+				fvWekaAttributes.addElement(Attribute1);
+				fvWekaAttributes.addElement(Attribute2);
+				fvWekaAttributes.addElement(Attribute3);
+				fvWekaAttributes.addElement(Attribute4);
+				fvWekaAttributes.addElement(Attribute5);
+				fvWekaAttributes.addElement(Attribute6);
+				fvWekaAttributes.addElement(Attribute7);
+				fvWekaAttributes.addElement(Attribute8);
+				fvWekaAttributes.addElement(Attribute9);
+				fvWekaAttributes.addElement(ClassAttribute);
+
+				Instances isTrainingSet = new Instances("Rel", fvWekaAttributes, 10);
+				isTrainingSet.setClassIndex(9);
+
+				Instance instance = new DenseInstance(10);
+
+				int[][] neighbourhood = chessboard.getChessBoardNeighbourhood(ant.getAntPosition(), visualField);
+				for (int r = 0; r < neighbourhood.length; r++) {
+					for (int c = 0; c < neighbourhood.length; c++) {
+						instance.setValue(r * neighbourhood.length + c, neighbourhood[r][c]);
+						// instance.setValue(r*neighbourhood.length + c, neighbourhood[r][c]);
+					}
+				}
+				try {
+					// Instances isTrainingSet = new Instances("Rel", fvWekaAttributes, 10);
+					instance.setDataset(isTrainingSet);
+					System.out.println("instance => "  + instance);
+					double[] fDistribution = classifier.distributionForInstance(instance);
+					
+					double max = 0;
+					int index = -1;
+					for (int i = 0; i < fDistribution.length; i++) {
+						System.out.println("fDistribution[i]=" + fDistribution[i]);
+						if (fDistribution[i] > max) {
+							index = i;
+							max = fDistribution[i];
+						}
+					}
+					System.out.println("direction : " + index + ", likelihood : " + max);
+					switch (index) {
+					case 0:
+						arg0.setKeyCode(KeyEvent.VK_UP);
+						break;
+					case 1:
+						arg0.setKeyCode(KeyEvent.VK_DOWN);
+						break;
+					case 2:
+						arg0.setKeyCode(KeyEvent.VK_LEFT);
+						break;
+					case 3:
+						arg0.setKeyCode(KeyEvent.VK_RIGHT);
+						break;
+					default:
+						break;
+					}
+
+					// classifier.classifyInstance(instance);
+				} catch (Exception e) {
+					throwedException = e;
+				}
+			} else
+				return;
+		}
 
 			switch (arg0.getKeyCode()) {
 			case KeyEvent.VK_UP:
@@ -212,7 +302,7 @@ public class GamePlay extends JPanel implements KeyListener {
 			default:
 				break;
 			}
-		}
+		
 
 		if (posChanged) {
 			Direction keyCode = Direction.getDirectionFromCode(arg0.getKeyCode());
